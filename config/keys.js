5fd3a29e95d5373ceee8acdbfd6489f2750120e5
Anders Strömberg <40022844+anst9000@users.noreply.github.com>
2020-02-05 22:28:10 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:adminpassword@cluster0-rgug5.mongodb.net/barnsley_fc?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://<USER>:<PASSWORD>@cluster0-rgug5.mongodb.net/barnsley_fc?retryWrites=true&w=majority'
 }
-
-  // MONGO_DB_HOST=cluster0-rgug5.mongodb.net
-  // MONGO_DB_DATABASE=barnsley_fc
-  // MONGO_DB_USER=admin
-  // MONGO_DB_PASS=adminpassword
